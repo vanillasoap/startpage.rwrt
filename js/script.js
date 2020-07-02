9bc86065df6eca291831f106cf0346fd3d0a5efd
vanillasoap <soheilhivadnabi@gmail.com>
2020-07-02 03:12:40 +0200

    Added multi search

--- js/script.js
+++ js/script.js
@@ -32,241 +32,357 @@
 	THE SOFTWARE.
 */
 
-/* Settings *\
-\*==========*/
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* Settings */
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 var settings = {
-	"navigation": {
-		"newWindow": true
-	},
-	
-	"clock": {
-		"showClock": false
-	},
+    "navigation": {
+        "newWindow": true
+    },
 
-	"animation": {
-		"hideLinks": true
-	},
+    "search": {
+        "ShowSearch": true
+    },
 
-	"icons": {
-		"showIcons": true
-	}
+    "clock": {
+        "showClock": true
+    },
+
+    "animation": {
+        "hideLinks": true
+    },
+
+    "icons": {
+        "showIcons": false
+    }
 };
 
-/*  Clock  *\
-\*=========*/
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* Clock */
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 function updateClock() {
-	var currentTime = new Date ();
-	var currentHours = currentTime.getHours ();
-	var currentMinutes = currentTime.getMinutes ();
-	var currentSeconds = currentTime.getSeconds ();
+    var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    var currentSeconds = currentTime.getSeconds();
 
-	// Pad the minutes and seconds with leading zeros, if required
-	currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-	currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+    currentHours = (currentHours > 24) ? currentHours - 24 : currentHours;
+    currentHours = (currentHours == 0) ? 24 : currentHours;
 
-	// Choose either "AM" or "PM" as appropriate
-	var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+    // Compose the string for display
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " "
 
-	// Convert the hours component to 12-hour format if needed
-	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
 
-	// Convert an hours component of "0" to "12"
-	currentHours = (currentHours == 0) ? 12 : currentHours;
-
-	// Compose the string for display
-	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-	// Fill '#clock' div with time
-	$("#clock").html(currentTimeString);
+    $("#clock")
+        .html(currentTimeString);
 }
 
-function searchBox(url, name, placeholder) {
-	var string = '<form method="get" action="' + url + '">'
-	           + '<input type="text" id="g" name="' + name + '" placeholder="' + placeholder + '" maxlength="255" value="">'
-	           + '<input type="submit" value="Go">'
-	           + '</form>';
-	return string;
-}
+$(document)
+    .ready(function() {
 
+        var shortcuts = {};
+
+        /*  Get Links  */
+        var linkString = $('body')
+            .text();
+
+        /*  Clear Page  */
+        $('body')
+            .empty();
+
+        /*  Create Array from linkString  */
+        var linkArray = linkString.split("\n");
+
+        /*  Go thru Array  */
+        var i;
+        var count = 1;
+        var html = '';
+
+        for (i in linkArray) {
+
+            /*  Get line  */
+            var line = jQuery.trim(linkArray[i]);
+
+            // If line is empty, skip
+            if (!line) continue;
+
+            /*  If it doesn't contain "://", it's not a URL */
+            if (/:\/\//.test(line) != true) {
+                if (count > 1) {
+                    html = html + '</div>';
+                }
+                html = html + '<div class="block"><h1>' + line + '</h1><ul>';
+                count++;
+                continue;
+            }
+
+            /*  Split URL, Title and icon (if any) */
+            var lineArray = line.split(" || ");
+            var url = lineArray[0];
+            var title = lineArray[1];
+
+            var icon = "";
+            if (lineArray[3]) {
+                icon = lineArray[3];
+            }
+
+            /*  Add to shortcuts array *\
+            \*=========================*/
+            if (lineArray[2]) {
+                shortcuts[lineArray[2]] = "'" + url + "'";
+            }
+
+            /* Prepares HTML code for showing icon */
+            var iconHtml = '';
+            if (settings.icons.showIcons && icon) {
+                iconHtml = '<img src="' + icon + '"/>';
+            }
+
+            /*  Add HTML code  */
+            if (settings.navigation.newWindow) {
+                html = html + '<li>' + iconHtml + '<a href="' + url + '" target="_blank">' + title + '</a></li>'
+            } else {
+                html = html + '<li>' + iconHtml + '<a href="' + url + '">' + title + '</a></li>'
+            }
+        }
+
+        /*  Add generated content to page  */
+        html = html + '</ul></div>';
+        $('body')
+            .append(html);
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+        /* Animation */
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+        /*  Hide lists  */
+        if (settings.animation.hideLinks) {
+            $('ul')
+                .slideUp();
+
+            /*  Show on hover  */
+            $('.block')
+                .mouseenter(function() {
+                    $('ul', this)
+                        .stop()
+                        .slideDown();
+                });
+
+            /*  Hide on unhover  */
+            $('.block')
+                .mouseleave(function() {
+                    $('ul', this)
+                        .stop()
+                        .slideUp();
+                });
+        }
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+        /* Search */
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */        
+
+        if (settings.search.ShowSearch) {
+            $('body')
+                .append(form);
+            var form = document.createElement("form");
+            form.setAttribute("class", "search");
+            form.setAttribute("id", "js-search");
+            form.setAttribute("autocomplete", "off");
+            var input = document.createElement("input");
+            input.setAttribute("class", "search__text");
+            input.setAttribute("id", "js-search-text");
+            input.setAttribute("contenteditable", "true");
+            input.setAttribute("type", "text");
+            input.setAttribute("placeholder", "🔍");            
+            input.setAttribute("autofocus", "")
+            form.appendChild(input);
+            document.body.appendChild(form);
+            var pre = document.createElement("help")
+            help.setAttribute("class", "help");
+            help.setAttribute("id", "js-help");
+
+        }
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+        /* Clock */
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+        if (settings.clock.showClock) {
+            // Add empty '#clock' div
+            $('body')
+                .append('<div id="clock"></div>');
+
+            // Update clock
+            setInterval('updateClock()', 1000);
+        }
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+        /* Keybindings */
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+        var typed = '';
+        var shortcutArray = Object.keys(shortcuts);
+        var typedDate = new Date();
+
+        // Check if we typed a keybinding
+        function hasSubstring(element) {
+            var index = typed.indexOf(element);
+            if (index >= 0) {
+                var sliced = typed.slice(index, typed.length);
+                typed = ''; // Clean typed, so that we can watch for the next keybinding
+                if (settings.navigation.newWindow) {
+                    window.open(eval(shortcuts[sliced]));
+                } else {
+                    window.location.replace(eval(shortcuts[sliced]));
+                }
+            }
+        }
+
+    });
+
+
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* Search
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 $(document).ready(function() {
+    var Cmdr = {
+        searchForm: document.getElementById('js-search'),
+        searchText: document.getElementById('js-search-text'),
+        searchHelp: document.getElementById('js-help'),
 
-	var shortcuts = {};
-	
-	/*  Get Links  *\
-	\*=============*/
-	var linkString = $('body').text();
+        init: function(opts) {
+            Cmdr.default = opts.default;
+            Cmdr.commands = opts.commands;
+            Cmdr.searchForm.addEventListener('submit', Cmdr.search, false);
+        },
 
-	/*  Clear Page  *\
-	\*==============*/
-	$('body').empty();
+        search: function(e) {
+            var q = Cmdr.searchText.value;
 
-	/*  Create Array from linkString  *\
-	\*================================*/
-	var linkArray = linkString.split("\n");
+            if (q === '?') {
+                Cmdr.commands.forEach(function(command) {
+                    Cmdr.searchHelp.innerHTML += command.key + ': ' + command.name + '\n';
+                });
 
-	/*  Go thru Array  *\
-	\*=================*/
-	var i;
-	var count = 1;
-	var html = '';
+                Cmdr.searchHelp.style.height = Math.ceil(Cmdr.commands.length / 2) + 'rem';
+                Cmdr.searchText.value = '';
+            } else {
+                Cmdr.location = Cmdr.default+encodeURIComponent(q);
+                q = q.split(':');
 
-	for(i in linkArray) {
+                Cmdr.commands.forEach(function(command) {
+                    if (q[0] === command.key) {
+                        Cmdr.location = command.url;
 
-		/*  Get line  *\
-		\*============*/
-		var line = jQuery.trim(linkArray[i]);
-
-		// If line is empty, skip
-		if(!line) continue;
-
-		/*  If it doesn't contain "://",  *\
-		|*  it's not a URL                *|
-		\*================================*/
-		if(/:\/\//.test(line) != true) {
-			if(count > 1) {
-				html = html + '</div>';
-			}
-			html = html + '<div class="block"><h1>' + line + '</h1><ul>';
-			count++;
-			continue;
-		}
-
-		/*  Split URL, Title and icon (if any) *\
-		\*=======================*/
-		var lineArray = line.split(" || ");
-		var url = lineArray[0];
-		var title = lineArray[1];
-		
-		var icon = "";
-		if (lineArray[3]) {
-			icon = lineArray[3];
-		}
-		
-		/*  Add to shortcuts array *\
-		\*=========================*/
-		if(lineArray[2]) {
-			shortcuts[lineArray[2]] = "'"+url+"'";
-		}
-
-		/* Prepares HTML code for showing icon *\
-		\*=====================================*/
-		var iconHtml = '';
-		if (settings.icons.showIcons && icon) {
-			iconHtml = '<img src="' + icon + '"/>'; 
-		}
-
-		/*  Add HTML code  *\
-		\*=================*/
-		if(settings.navigation.newWindow) {
-			html = html + '<li>' + iconHtml + '<a href="' + url + '" target="_blank">' + title + '</a></li>'
-		}
-		else {
-			html = html + '<li>' + iconHtml + '<a href="' + url + '">' + title + '</a></li>'
-		}
-	}
-
-	/*  Add generated content to page  *\
-	\*=================================*/
-	html = html + '</ul></div>';
-	$('body').append(html);
-
-
-	/*  Animation Time!  *\
-	\*===================*/
-	
-	/*  Hide lists  *\
-	\*==============*/
-	if (settings.animation.hideLinks) {
-		$('ul').slideUp();
-
-		/*  Show on hover  *\
-		\*=================*/
-		$('.block').mouseenter(function() {
-			$('ul', this).stop().slideDown();
-		});
-
-		/*  Hide on unhover  *\
-		\*===================*/
-		$('.block').mouseleave(function() {
-			$('ul', this).stop().slideUp();
-		});
-	}
-
-
-	/*  Search Engines  *\
-	\*==================*/
-
-	var search = '<div id="searches">';
-
-	for (var i = 0; i < settings.search.engines.length; i++) {
-		var engine = settings.search.engines[i];
-		search = search + searchBox(engine[0], engine[1], engine[2]);
-		if(engine[3]) {
-			var jsSearchUrl=engine[0]+"?"+engine[1]+"=";
-			var jsSearchPrompt="prompt('Search "+engine[2]+":')";
-			var jsSearch="'"+jsSearchUrl+"'+"+jsSearchPrompt;
-			shortcuts[engine[3]] = jsSearch;
-      }
-	}
-
-	search = search + '</div>';
-
-	/*  Add to page  *\
-	\*===============*/
-	$('body').append(search);
-	if(settings.search.focusSearch) {
-		var searchDiv = document.getElementById ('searches');
-		$(searchDiv.firstChild.firstChild).focus();
-	}
- 
-	/*  Clock  *\
-	\*=========*/
-
-	if(settings.clock.showClock) {
-		// Add empty '#clock' div
-		$('body').append('<div id="clock"></div>');
-
-		// Update clock
-		setInterval('updateClock()', 1000);
-	}
-
-
-	/*  Keybindings  *\
-	\*===============*/
-
-	var typed = '';
-	var shortcutArray = Object.keys(shortcuts);
-	var typedDate = new Date();
-		
-	// Check if we typed a keybinding
-	function hasSubstring(element) {
-		var index = typed.indexOf(element);
-		if(index >= 0) {
-			var sliced = typed.slice(index, typed.length);
-			typed = ''; // Clean typed, so that we can watch for the next keybinding
-			if(settings.navigation.newWindow) {
-				window.open(eval(shortcuts[sliced]));
-			} else {
-				window.location.replace(eval(shortcuts[sliced]));
-			}
-		}
-	}
-
-	// React on keypress
-	$(window).keypress(function(e) {
-		// If we're in an input, we don't want to interpret the keypresses
-		$('input').keypress(function(e) {
-			e.stopPropagation();
-		});
-		var nowDate = new Date();
-		var diffMs = (nowDate - typedDate);
-		if (diffMs > 1000) {	
-			typed = String.fromCharCode(e.which);
-		} else {
-			typed = typed + String.fromCharCode(e.which);
-		}
-		typedDate = new Date();
-		shortcutArray.some(hasSubstring);
-	});
-
-});
+                        if (q[1] && command.search) {
+                            q.shift();
+                            var searchText = encodeURIComponent(q.join(':')
+                                .trim());
+                            Cmdr.location = command.url + command.search + searchText;
+                        }
+                    }
+                });
+                window.location.href = Cmdr.location;
+            }
+            e.preventDefault();
+        }
+    };
+    Cmdr.init({
+        default: 'https://www.google.com/search?q=',
+        commands: [{
+                key: 'a',
+                name: 'Amazon',
+                url: 'https://www.amazon.com',
+                search: '/s/?field-keywords='
+            },
+            {
+                key: 'c',
+                name: 'Coinbase',
+                url: 'https://www.coinbase.com'
+            },
+            {
+                key: 'd',
+                name: 'Drive',
+                url: 'https://drive.google.com',
+                search: '/drive/search?q='
+            },
+            {
+                key: 'f',
+                name: 'Facebook',
+                url: 'https://www.facebook.com',
+                search: '/search/top/?q='
+            },
+            {
+                key: 'g',
+                name: 'GitHub',
+                url: 'https://github.com',
+                search: '/search?q='
+            },
+            {
+                key: 'h',
+                name: 'Hacker News',
+                url: 'https://hn.algolia.com',
+                search: '/?query='
+            },
+            {
+                key: 'i',
+                name: 'Inbox',
+                url: 'https://inbox.google.com',
+                search: '/search/'
+            },
+            {
+                key: 'k',
+                name: 'Keep',
+                url: 'https://keep.google.com',
+                search: '/#search/text='
+            },
+            {
+                key: 'p',
+                name: 'Product Hunt',
+                url: 'https://www.producthunt.com',
+                search: '/search?q='
+            },
+            {
+                key: 'r',
+                name: 'Reddit',
+                url: 'https://www.reddit.com',
+                search: '/search?q='
+            },
+            {
+                key: 's',
+                name: 'SoundCloud',
+                url: 'https://soundcloud.com',
+                search: '/search?q='
+            },
+            {
+                key: 't',
+                name: 'Twitter',
+                url: 'https://twitter.com',
+                search: '/search?q='
+            },
+            {
+                key: 'y',
+                name: 'YouTube',
+                url: 'https://www.youtube.com',
+                search: '/results?search_query='
+            },
+            {
+                key: 'Y',
+                name: 'YTS',
+                url: 'https://yts.ag'
+            },
+            {
+                key: '3',
+                name: 'localhost:3000',
+                url: 'http://localhost:3000'
+            },
+            {
+                key: '9',
+                name: 'localhost:9000',
+                url: 'http://localhost:9000'
+            },
+        ]
+    });
+})();
